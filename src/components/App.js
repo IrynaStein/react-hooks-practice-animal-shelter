584a2d17b833f06346e899328a9fc36fadf767d5
Iryna Stein <isophiestein@gmail.com>
2021-06-26 12:27:51 -0600

    Fimished Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,41 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pets")
+    .then (resp => resp.json())
+    .then (data => setPets(data))
+  }, [])
+
+  function onChangeType(filterType){
+    console.log(filterType)
+    setFilters({type: filterType})
+    const filteredPets = pets.filter((pet) => pet.type === filterType)
+    setPets(filteredPets)
+  }
+console.log(pets)
+
+function onFindPetsClick(){
+  console.log(`Looking for pets`)
+  console.log(Object.values(filters))
+ if (Object.values(filters).toString() === "all"){
+  fetch("http://localhost:3001/pets")
+  .then (resp => resp.json())
+  .then (data => setPets(data))
+ }
+ else {
+  fetch(`http://localhost:3001/pets?type=${Object.values(filters).toString()}`)
+  .then (resp => resp.json())
+  .then (data => setPets(data))
+ } 
+}
+
+function onAdoptPet(id){
+  console.log(id)
+  const adoptedPets = pets.filter((pet) => pet.id !== id)
+  setPets(adoptedPets)
+}
 
   return (
     <div className="ui container">
@@ -15,10 +50,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
